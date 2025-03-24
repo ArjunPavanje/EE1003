4accbab7d48a503f1e4382077becdb3ae85c6ce4
ArjunPavanje <arjupavanje@gmail.com>
2025-03-24 22:37:18 +0530

    assembly codee

--- Hardware/Project/Clock/main.asm
+++ Hardware/Project/Clock/main.asm
@@ -9,64 +9,17 @@
 ;  out TCCR0B, r16 
   ldi r16, 0x00 ; seconds (ones)
   ldi r17, 0x00 ; seconds (tens)
-  ldi r18, 0x01 ; minutes (ones)
-  ldi r19, 0x03 ; minutes (tens)
-  ldi r20, 0x04 ; hours (ones)
-  ldi r21, 0x01 ; hours (tens)
+  ldi r18, 0x05 ; minutes (ones)
+  ldi r19, 0x04 ; minutes (tens)
+  ldi r20, 0x03 ; hours (ones)
+  ldi r21, 0x00 ; hours (tens)
 loop:
 
 
 
-  ; ;  minutes (ones)
-  ;  lsl r18
-  ;  lsl r18
-  ;  ldi r24, 0x03
-  ;  out PORTB, r24
-  ;  out PORTD, r18
-  ;  lsr r18
-  ;  lsr r18
-  ;
-  ; ; minutes (tens)
-  ;  lsl r19
-  ;  lsl r19
-  ;  ldi r24, 0x04
-  ;  out PORTB, r24
-  ;  out PORTD, r19
-  ;  lsr r19
-  ;  lsr r19
-  ;
-  ; ; hours (ones)
-  ;  lsl r20
-  ;  lsl r20
-  ;  ldi r24, 0x05
-  ;  out PORTB, r24
-  ;  out PORTD, r20
-  ;  lsr r20
-  ;  lsr r20
-  ;
-  ; ;  hours (tens)
-  ;  lsl r21
-  ;  lsl r21
-  ;  ldi r24, 0x06
-  ;  out PORTB, r24
-  ;  out PORTD, r21
-  ;  lsr r21
-  ;  lsr r21
-
-  ;rcall second_ones
-  ;rcall second_tens
-  ;rcall hour_tens
-
-
-  ;ldi r24, 0b01000000 ;times to run the loop = 64 for 1 second delay
-  ;rcall PAUSE 		;call the PAUSE label
-
-
-  ; Assuming a 16 MHz clock frequency (1 clock cycle = 62.5 ns)
-
-  ldi r28, 0x08          ; Load r18 with a value to count down (128 iterations)
-  ldi r29, 0x08          ; Load r19 with 128 (inner loop counter)
-  ldi r30, 0x08          ; Load r20 with 128 (middle loop counter)
+    ldi r28, 0x08          ; Load r18 with a value to count down (128 iterations)
+  ldi r29, 0x04          ; Load r19 with 128 (inner loop counter)
+  ldi r30, 0x02          ; Load r20 with 128 (middle loop counter)
 
 delay_loop:
   ; Display ones (seconds)
@@ -161,7 +114,7 @@ delay_loop:
 second_ones:
 
   ; W, X, Y, Z = r27, r28, r29, r30
-
+  ldi r23, 0x00
   ldi r24, 0x01
   ; W
   mov r27, r16
@@ -185,6 +138,31 @@ second_ones:
 
   ;ldi r24, 0x01
   clr r16
+
+;  mov r22, r27
+;  eor r22, r24
+;  ;add r23, r22
+;  lsl r24
+;  mov r25, r28
+;  eor r25, r24
+;  and r22, r25
+;  ;add r23, r22
+;  lsl r24
+;  mov r25, r29
+;  eor r22, r24
+;  add r23, r22
+;  lsl r24
+;  mov r22, r30
+;  eor r22, r24
+;  add r23, r24
+;  
+;  mov r22, r23
+;  lsl r22
+;  lsl r22
+;  lsl r22
+;  lsl r22
+
+  ldi r24, 0x01	
 
   ; A = !W
   mov r26, r27
@@ -281,77 +259,15 @@ second_ones:
 
   add r16, r26
 
-;  cpi r16, 0x00
-;  breq second_tens ; might have to replace ret with rjmp loop
-;  if(C)
-;    W = A
-;  else
-;    W = B
-;  W = AC + B(C*)
-  
-;  if (second_ones == 0):
-;    r22 = r17
-;  else:
-;    r22 = r22
-;  ldi r17, r22
-
-  ; C -> r23, C* -> r24
-  ; r23, 0 if r16 is 0 1 otherwise
-
-  ldi r23, 0x00
-  ldi r24, 0b11111111
-  ldi r25, 0x00
-  add r24, r16
-  brvs Carry
-  ;or r23, r16
-  ;brne Carry
 
 
-;; Assume R16 contains the value to check
-;; R23 will be set to 0x00 if R16 is 0, and 0xFF if R16 is non-zero
-;
-;    ; Clear R23 (set it to 0x00)
-;    CLR     R23              ; R23 = 0x00
-;
-;    ; Perform a bitwise AND with itself to check if R16 is zero
-;    ; If R16 is 0, the result of R16 AND R16 will be 0; otherwise, it will be non-zero.
-;    MOV     R25, R16         ; Copy R16 to R25 (temporary register)
-;    AND     R25, R16         ; R25 = R16 & R16 (this just checks if R16 is 0 or non-zero)
-;
-;    ; Now, if R25 is non-zero (i.e., R16 was non-zero), set R23 to 0xFF
-;    ; We will use a bitwise OR to ensure R23 becomes 0xFF if R16 is non-zero
-;    OR      R23, R25         ; OR R23 with R25, if R16 was non-zero, this makes R23 = 0xFF
-
-    ; Done!
-
-;    MOV     r23, r16      ; Copy r16 into r23
-;    AND     r23, r23      ; Perform bitwise AND of r23 with itself (results in 0 if r16 is 0)
-;    COM     r23           ; Complement r23
-;  MOV     r23, r16      ; Copy r16 into r23
-;  OR      r23, r23      ; Perform OR of r23 with itself (sets Z flag if r16 is 0)
-;  COM     r23           ; Complement r23  mov     r23, r16      ; Copy the value of r16 into r23
-  
-;  OR      r23, r23      ; Perform bitwise OR of r23 with itself
-;  COM     r23           ; Complement r23 to invert the bits
-;  AND    r23, r24     ; Mask to keep r23 as an 8-bit value  rjmp loop
-
-  mov r24, r23
-  COM r24
-  
-  mov r22, r17
-  rcall second_tens
-
-  and r22, r24
-  and r17, r23
-
-  add r17, r22
 
 
-;
-;
-;  ldi r17, r22
-  ;ldi r22, r16
-  
+cpi r16, 0x00
+breq second_tens ; might have to replace ret with rjmp loop
+
+
+ rjmp loop 
 
 
  
@@ -388,65 +304,99 @@ second_tens:
 
 
 
-  ; B = (X && !Y) || (!X && Y && Z)
-
-  ; (X && !Y)
-  mov r26, r29
-  eor r26, r24
-  and r26, r28
-
-  ; (!X && Y && Z)
-  mov r25, r28
-  eor r25, r24
-  and r25, r29
-  and r25, r30
-
-  or r26, r25
-
-  lsl r26
-  lsl r26
-
-  add r17, r26
-
-  ; C = X'YZ' + Y'Z
-
-  ; X'YZ'
+  ; B = !XYZ
   mov r26, r28
   eor r26, r24
   and r26, r29
-  mov r25, r30
-  eor r25, r24
-  and r26, r25
+  and r26, r30
 
-  ; Y'Z
-  mov r15, r29
-  eor r15, r24
-  and r15, r30
-
-  or r26, r15
-
+  lsl r26
   lsl r26
 
   add r17, r26
 
-  ; D = Y'Z' + X'Z'
 
-  ; Y'Z'
-  mov r26, r29
+;
+;  ; (X && !Y)
+;  mov r26, r29
+;  eor r26, r24
+;  and r26, r28
+;
+;  ; (!X && Y && Z)
+;  mov r25, r28
+;  eor r25, r24
+;  and r25, r29
+;  and r25, r30
+;
+;  or r26, r25
+;
+;  lsl r26
+;  lsl r26
+;
+;  add r17, r26
+;
+  ; C = X'YZ' + Y'Z
+
+  mov r26, r28
   eor r26, r24
   mov r25, r30
   eor r25, r24
-  and r26, r25
+  and r25, r26
+  
+  add r17, r25
 
-  ; X'Z'
-  mov r15, r28
+  and r25, r29
+  
+
+  mov r15, r29
   eor r15, r24
-  and r25, r15
+
+  and r26, r15
+  and r26, r30
 
   or r26, r25
+  lsl r26
 
   add r17, r26
+  
 
+;  ; X'YZ'
+;  mov r26, r28
+;  eor r26, r24
+;  and r26, r29
+;  mov r25, r30
+;  eor r25, r24
+;  and r26, r25
+;
+;  ; Y'Z
+;  mov r15, r29
+;  eor r15, r24
+;  and r15, r30
+;
+;  or r26, r15
+;
+;  lsl r26
+;
+;  add r17, r26
+;
+  ; D = Y'Z' + X'Z'
+
+;  ; Y'Z'
+;  mov r26, r29
+;  eor r26, r24
+;  mov r25, r30
+;  eor r25, r24
+;  and r26, r25
+;
+;  ; X'Z'
+;  mov r15, r28
+;  eor r15, r24
+;  and r25, r15
+;
+;  or r26, r25
+;
+;  add r17, r26
+;
   cpi r17, 0x00
   breq minute_ones ; might have to replace ret with rjmp loop
 
@@ -701,102 +651,118 @@ hour_ones:
 
   ;ldi r24, 0x01
   clr r20
+  
+  mov r26, r29
+  eor r26, r24
+  and r26, r30
 
-  ; A = !W
-  mov r26, r27
-  eor r26, r24 
+  lsl r26
   add r20, r26
 
-  ; B = (!W && X && !Z) || (W && !X && !Z)
+  mov r26, r29
+  eor r26, r24
 
-  ; (!W && X && !Z)
-  and r26, r28 
   mov r25, r30
-  eor r25, r24 
+  eor r25, r24
   and r26, r25
 
-  ; (W && !X && !Z)
-  and r25, r27
-  mov r15, r28
-  eor r15, r24 
-  and r25, r15
-
-  or r26, r25
-
-  lsl r26 
-  add r20, r26
-  ; C = (!W && Y && !Z) || (!X && Y && !Z) || (W && X && !Y && !Z)
-
-  ; (!W && Y && !Z)
-  mov r26, r27 
-  eor r26, r24 
-  and r26, r29
-  mov r25, r30
-  eor r25, r24 
-  and r26, r25 
-
-  ; (!X && Y && !Z)
-  ;and r25, r29
-
-  mov r15, r28 
-  eor r15, r24
-  and r15, r29
-  and r15, r25
-  ;and r25, r15
-
-  or r26, r15 
-
-  ; (W && X && !Y && !Z)
-
-  ;  mov r25, r30
-  ;  eor r25, r24 
-  ;  and r25, r27
-  ;  and r25, r28
-  ;  mov r15, r29 
-  ;  eor r15, r24 
-  ;  and r25, r15
-
-  mov r15, r29
-  eor r15, r24
-  and r15, r25
-  and r15, r27
-  and r15, r28
-
-  or r26, r15
-
-  lsl r26
-  lsl r26
-
   add r20, r26
 
-  ; D = (W && X && Y && !Z) || (!W && !X && !Y && Z)
-
-  ; (W && X && Y && !Z)
-  mov r26, r30
-  eor r26, r24 
-  and r26, r27 
-  and r26, r28
-  and r26, r29
-
-  ; (!W && !X && !Y && Z)
-  mov r25, r27
-  eor r25, r24 
-  and r25, r30
-  mov r15, r28
-  eor r15, r24 
-  and r25, r15
-  mov r15, r29
-  eor r15, r24 
-  and r25, r15
-
-  or r26, r25
-
-  lsl r26
-  lsl r26
-  lsl r26
-
-  add r20, r26
-
+;  ; A = !W
+;  mov r26, r27
+;  eor r26, r24 
+;  add r20, r26
+;
+;  ; B = (!W && X && !Z) || (W && !X && !Z)
+;
+;  ; (!W && X && !Z)
+;  and r26, r28 
+;  mov r25, r30
+;  eor r25, r24 
+;  and r26, r25
+;
+;  ; (W && !X && !Z)
+;  and r25, r27
+;  mov r15, r28
+;  eor r15, r24 
+;  and r25, r15
+;
+;  or r26, r25
+;
+;  lsl r26 
+;  add r20, r26
+;  ; C = (!W && Y && !Z) || (!X && Y && !Z) || (W && X && !Y && !Z)
+;
+;  ; (!W && Y && !Z)
+;  mov r26, r27 
+;  eor r26, r24 
+;  and r26, r29
+;  mov r25, r30
+;  eor r25, r24 
+;  and r26, r25 
+;
+;  ; (!X && Y && !Z)
+;  ;and r25, r29
+;
+;  mov r15, r28 
+;  eor r15, r24
+;  and r15, r29
+;  and r15, r25
+;  ;and r25, r15
+;
+;  or r26, r15 
+;
+;  ; (W && X && !Y && !Z)
+;
+;  ;  mov r25, r30
+;  ;  eor r25, r24 
+;  ;  and r25, r27
+;  ;  and r25, r28
+;  ;  mov r15, r29 
+;  ;  eor r15, r24 
+;  ;  and r25, r15
+;
+;  mov r15, r29
+;  eor r15, r24
+;  and r15, r25
+;  and r15, r27
+;  and r15, r28
+;
+;  or r26, r15
+;
+;  lsl r26
+;  lsl r26
+;
+;  add r20, r26
+;
+;  ; D = (W && X && Y && !Z) || (!W && !X && !Y && Z)
+;
+;  ; (W && X && Y && !Z)
+;  mov r26, r30
+;  eor r26, r24 
+;  and r26, r27 
+;  and r26, r28
+;  and r26, r29
+;
+;  ; (!W && !X && !Y && Z)
+;  mov r25, r27
+;  eor r25, r24 
+;  and r25, r30
+;  mov r15, r28
+;  eor r15, r24 
+;  and r25, r15
+;  mov r15, r29
+;  eor r15, r24 
+;  and r25, r15
+;
+;  or r26, r25
+;
+;  lsl r26
+;  lsl r26
+;  lsl r26
+;
+;  add r20, r26
+;
   cpi r20, 0x00
   breq hour_tens ; might have to replace ret with rjmp loop
 
@@ -850,8 +816,6 @@ hour_tens:
   ;  lsr r16
   ;  ret
 
-Carry:
-  mov r23, r24
 
 
 
